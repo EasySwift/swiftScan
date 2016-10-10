d5991d1816c1e00201ca2c987507e1fa9853a797
stubbornnessness <1219721814yxj>
2016-10-11 00:18:01 +0800

    1.1版本支持carthage

---
+++ SwiftScan_iOS/SwiftScan_iOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftScan_iOS.h
+//  SwiftScan_iOS
+//
+//  Created by yuanxiaojun on 2016/10/11.
+//  Copyright © 2016年 xialibing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftScan_iOS.
+FOUNDATION_EXPORT double SwiftScan_iOSVersionNumber;
+
+//! Project version string for SwiftScan_iOS.
+FOUNDATION_EXPORT const unsigned char SwiftScan_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftScan_iOS/PublicHeader.h>
+
+
